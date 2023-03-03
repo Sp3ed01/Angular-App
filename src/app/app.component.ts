fed1ee31e5555cda7817e4aac4574208607313a1
Chilom Mihai <mihai.chilom@yahoo.com>
2023-03-03 13:12:38 +0000

    ex-view-child-children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  @ViewChild('dobinput')
+  dateOfBirth!: ElementRef;
+  @ViewChild('ageinput')
+  age!: ElementRef;
+  @ViewChild(DemoComponent, { static: true })
+  demoComponent!: DemoComponent;
+  @ViewChildren('inputFields') inputFields!: QueryList<any>;
+
+  calculateAge() {
+    let birthYear = new Date(this.dateOfBirth?.nativeElement.value).getFullYear();
+    let currentYewr = new Date().getFullYear();
+    let age = currentYewr - birthYear;
+    this.age.nativeElement.value = age;
+  }
+
+  items = [
+    { value: 'Item 1' },
+    { value: 'Item 2' },
+    { value: 'Item 3' },
+  ];
+
+  logValues() {
+    this.inputFields.forEach((input) => console.log(input.nativeElement.value));
+  }
 }
