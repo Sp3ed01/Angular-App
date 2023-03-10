030d053d0f2fe34a36d43a95e601bbf105d82c48
Chilom Mihai <mihai.chilom@yahoo.com>
2023-03-10 07:37:48 +0000

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component, ContentChild, ElementRef, ViewChild } from '@angular/core';
+import { Component, ContentChild, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { UserServiceService } from './user-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private currentUserService: UserServiceService) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.currentUserService.setCurrentUser();
+    }, 10000);
+    this.currentUserService.setCurrentUser();
+  }
   title: string = 'Hello from Craiova';
   items: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Route, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
+import { UserServiceService } from './user-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate{
+export class AuthGuardService implements CanActivate {
 
-  canActivate():boolean{
-    if(localStorage.getItem("token"))
+  constructor(private currentUserService: UserServiceService, private router: Router) { }
+  
+  canActivate(): Observable<boolean> {
+    return this.currentUserService.currentUser$.pipe(
+      filter(currentUser => currentUser !== undefined),
+      map((currentUser)=> {
+      if(!currentUser) {
+        this.router.navigateByUrl('/');
+        return false;
+      }
       return true;
-    return false;
+    })
+    )
   }
 
-  constructor() { }
+  // canActivate():boolean{
+  //   if(localStorage.getItem("token"))
+  //     return true;
+  //   return false;
+  // }
+
 }
